63f23ad8249df6efdb555856896b7675a7a8b3a7
p.b <p.b@restissecret>
2025-04-16 07:08:38 +0200

    Adding servicebus DI

--- src/rest_api/Program.cs
+++ src/rest_api/Program.cs
@@ -1,9 +1,18 @@
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Azure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddServiceBusClient(builder.Configuration.GetConnectionString("ConnectionStrings:ServiceBus"));
+});
+
 
 var app = builder.Build();
 
@@ -16,29 +25,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapPost("/sendhellocommand", static async (IAzureClientFactory<ServiceBusSender> senderFactory)  =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var sender = senderFactory.CreateClient("HelloCommandQueue");
+    await sender.SendMessageAsync(new ServiceBusMessage("Hello World!"));
+    return Results.Ok();
 })
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+.WithName("SendHelloCommand")
+.WithDescription("Sends a hello command to the queue.");
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
